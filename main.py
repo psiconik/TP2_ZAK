a3981dc9ba81ba5ec3bfe5179ae61d3d56a755e1
kiralyz <kiralyz@educ.college-montreal.qc.ca>
2023-10-12 14:47:58 -0400

    bf

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 # Zak Alexandre Kiraly
 # 12 sept 2023
 # TP2
+
 import random
 
-while
-    chiffre_aleatoire = random.randint(1, 1000)
+recommencer = True
 
+#boucle qui permet a l'utilisateur de recommencer le jeu
+while(recommencer == True):
+    chiffre_aleatoire = random.randint(1, 1000)
+    print(chiffre_aleatoire)
     reponse = False
 
+    #boucle qui permet a l'utilisateur de
     while reponse == False:
         guessed_number = int(input("quel est ton essait (nombre)? \n"))
 
@@ -22,4 +27,7 @@ while
             reponse = True
 
     choix_rec = str(input('veux-tu recommencer? o/n \n'))
-    if choix_rec == o:
+    if choix_rec == 'o':
+        recommencer = True
+    else:
+        recommencer = False
